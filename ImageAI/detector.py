dbc3c2240b75d378e4df1548e5e3356801798fdb
Monalika P <57065882+Monalika-P@users.noreply.github.com>
2020-10-19 10:47:00 +0530

    Add files via upload

---
+++ ImageAI/detector.py
@@ -0,0 +1,23 @@
+# Importing the libraries
+from imageai.Detection import ObjectDetection
+import os
+
+execution_path = os.getcwd()
+
+# Create an instance of the class ObjectDetection
+detector = ObjectDetection()
+
+# Calling the pre-trained models
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath(os.path.join(execution_path, "D:\Internship\Optm\Object_detection\models\coco_best_resnet50_v2.0.1.h5"))
+
+# Loading the models
+detector.loadModel()
+
+# Object Detection
+detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path, "D:\Internship\Optm\Object_detection\input\cd.jpg"),
+                                             output_image_path=os.path.join(execution_path, "D:\Internship\Optm\Object_detection\output\cd.jpg"))
+
+# Printing the dictionary containing name of each detected object along with its percentage probability
+for eachObject in detections:
+    print(eachObject["name"], " : ", eachObject["percentage_probability"])
